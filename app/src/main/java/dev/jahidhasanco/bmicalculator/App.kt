1e0d1577ed6583a5d35e02ce4e10177e03f5896a
loitp <www.muathu@gmail.com>
2023-05-01 20:40:32 +0700

    up

--- app/src/main/java/dev/jahidhasanco/bmicalculator/App.kt
+++ app/src/main/java/dev/jahidhasanco/bmicalculator/App.kt
@@ -3,7 +3,16 @@ package dev.jahidhasanco.bmicalculator
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class App: Application() {
+//TODO ic launcher
+//TODO rate app
+//TODO more app
+//TODO share app
+//TODO ad
+//TODO policy
+//TODO ad id manifest
+//TODO firebase
+//TODO keystore
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)

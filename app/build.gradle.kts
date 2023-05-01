d28987dc3f920774d7c850fc08fd9d6b70036687
loitp <www.muathu@gmail.com>
2023-05-01 20:39:38 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "dev.jahidhasanco.bmicalculator"
+        applicationId = "com.roy93group.bmicalculator"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 20230501
+        versionName = "2023.05.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -37,23 +37,22 @@ android {
         jvmTarget = "1.8"
     }
     namespace = "dev.jahidhasanco.bmicalculator"
-    android.buildFeatures.dataBinding=true
+    android.buildFeatures.dataBinding = true
 
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("com.github.adityagohad:HorizontalPicker:1.0.1")
     implementation("com.github.psuzn:WheelView:1.0.0")
     implementation("com.github.CNCoderX:WheelView:1.2.6")
     implementation("com.github.mhdmoh:swipe-button:1.0.3")
 
+    //for testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
@@ -8,13 +6,13 @@ plugins {
 }
 
 sonarqube {
-  properties {
-    property("sonar.projectKey", "JahidHasanCO_BMI-Calculator")
-    property("sonar.organization", "jahidhasanco")
-    property("sonar.host.url", "https://sonarcloud.io")
-  }
+    properties {
+        property("sonar.projectKey", "JahidHasanCO_BMI-Calculator")
+        property("sonar.organization", "jahidhasanco")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
-tasks.register("clean",Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }

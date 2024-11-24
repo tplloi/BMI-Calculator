901da8df65d1f9db19739e95dfb6d4ea92dd91b7
roy <roy93group@gmail.com>
2024-11-24 19:34:57 +0700

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,16 @@ plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20-RC" apply false
-    id("org.sonarqube") version "3.5.0.2730"
+//    id("org.sonarqube") version "3.5.0.2730"
 }
 
-sonarqube {
-    properties {
-        property("sonar.projectKey", "JahidHasanCO_BMI-Calculator")
-        property("sonar.organization", "jahidhasanco")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
+//sonarqube {
+//    properties {
+//        property("sonar.projectKey", "JahidHasanCO_BMI-Calculator")
+//        property("sonar.organization", "jahidhasanco")
+//        property("sonar.host.url", "https://sonarcloud.io")
+//    }
+//}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)

d04c26cd45851079d3a259b3a5675e1515a9ae25
roy <roy93group@gmail.com>
2024-12-18 16:30:08 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 21
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 20241215
-        versionName = "2024.12.15"
+        versionCode = 20241218
+        versionName = "2024.12.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
